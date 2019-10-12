fc7c88f715c21e0822f18aa16d8143cadb55f881
樱凌准 <SakuraSinojun@gmail.com>
2019-10-12 11:03:49 +0800

    update tips.

--- app/js/index/info.js
+++ app/js/index/info.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
             publish: function(event) {
                 __request("api.v1.info.update", {message: page.message, name: page.name, contact: page.contact}, function(res) {
                     console.debug(res);
-                    showTip("成功", "错误描述已上传，我们会尽快处理。<br />感谢您的配合，祝您生活愉快~");
+                    showTip("成功", "您的问题描述已上传，我们会尽快处理。<br />感谢您的配合，祝您生活愉快~");
                 });
             }
         }
